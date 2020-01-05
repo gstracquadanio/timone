872a1e7faef076700909b3c93fbed1efeebdb1bf
gstracqu <giovanni.stracquadanio@ed.ac.uk>
2020-01-05 19:27:52 +0000

    Bump version: 0.0.4 → 0.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = 'Giovanni Stracquadanio'
 author = 'Giovanni Stracquadanio'
 
 # The short X.Y version
-version = '0.0.4'
+version = '0.0.5'
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.5'
 
 
 # -- General configuration ---------------------------------------------------
