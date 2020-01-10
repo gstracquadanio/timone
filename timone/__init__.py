a7d42c2fb9882c7b5811397247110f36535315b6
gstracqu <giovanni.stracquadanio@ed.ac.uk>
2020-01-10 21:04:56 +0000

    Bump version: 0.3.1 → 0.3.2

--- timone/__init__.py
+++ timone/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 __author__ = """Giovanni Stracquadanio"""
 __email__ = 'dr.stracquadanio@gmail.com'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 # default values
 DEFAULT_ENDPOINT_URL="http://127.0.0.1:8000"
